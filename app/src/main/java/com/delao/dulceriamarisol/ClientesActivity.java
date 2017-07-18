7100a6cedb848b96c606c463c63a4d4386ecf7ca
jrivera <jrivera@JRIVERA>
2017-07-18 12:39:22 -0500

    Alta clientes

--- app/src/main/java/com/delao/dulceriamarisol/ClientesActivity.java
+++ app/src/main/java/com/delao/dulceriamarisol/ClientesActivity.java
@@ -2,6 +2,7 @@ package com.delao.dulceriamarisol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ClientesActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ClientesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientes);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Clientes");
     }
 }
