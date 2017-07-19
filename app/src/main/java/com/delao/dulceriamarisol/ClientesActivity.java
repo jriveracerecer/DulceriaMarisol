8f245d6d65c529fb561a7cddaa4bca8577b53930
jrivera <jrivera@JRIVERA>
2017-07-19 16:25:24 -0500

    Inserta cliente

--- app/src/main/java/com/delao/dulceriamarisol/ClientesActivity.java
+++ app/src/main/java/com/delao/dulceriamarisol/ClientesActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.delao.dulceriamarisol.data.DatabaseHandler;
+import com.delao.dulceriamarisol.models.Cliente;
 
 public class ClientesActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class ClientesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Clientes");
+
     }
 
     @Override
@@ -22,5 +27,22 @@ public class ClientesActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_item_guardar:
+                DatabaseHandler db = new DatabaseHandler(ClientesActivity.this, this);
+                Cliente c = new Cliente();
+                c.setNombre("Jesús");
+                c.setDireccion("Toluca");
+                c.setTelefono("7225489765");
+                db.addCliente(c);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/delao/dulceriamarisol/data/DatabaseHandler.java
+++ app/src/main/java/com/delao/dulceriamarisol/data/DatabaseHandler.java
@@ -1,8 +1,16 @@
 package com.delao.dulceriamarisol.data;
 
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.delao.dulceriamarisol.R;
+import com.delao.dulceriamarisol.models.Cliente;
 
 /**
  * Created by jrivera on 18/07/2017.
@@ -11,26 +19,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-
     private static final String DATABASE_NAME = "dulceriaMarisol";
-
     private static final String TABLE_CLIENTES = "clientes";
-
-    // Contacts Table Columns names
     private static final String KEY_ID = "id";
     private static final String KEY_NOMBRE = "nombre";
     private static final String KEY_DIRECCION = "direccion";
     private static final String KEY_TELEFONO = "telefono";
 
-    public DatabaseHandler(Context context){
+    private DatabaseHandler db;
+    private Context context;
+    private Activity activity;
+
+    public DatabaseHandler(Context context, Activity activity){
         super(context, DATABASE_NAME, null, 1);
+        this.db = this;
+        this.context = context;
+        this.activity = activity;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_CLIENTES_TABLE = "CREATE TABLE " + TABLE_CLIENTES + "("
                 + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NOMBRE + " TEXT,"
-                + KEY_DIRECCION + " TEXT," + KEY_TELEFONO + "TEXT" + ")";
+                + KEY_DIRECCION + " TEXT," + KEY_TELEFONO + " TEXT" + ")";
         db.execSQL(CREATE_CLIENTES_TABLE);
     }
 
@@ -41,5 +52,45 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         // Create tables again
         onCreate(db);
+    }
+
+    public void addCliente(Cliente cliente){
+        new InsertData().execute(cliente);
+    }
+
+    private class InsertData extends AsyncTask<Cliente, Void, Void>{
+
+        @Override
+            protected void onPreExecute() {
+                //super.onPreExecute();
+                activity.findViewById(R.id.nombreEditText).setEnabled(false);
+                activity.findViewById(R.id.direccionEditText).setEnabled(false);
+                activity.findViewById(R.id.telefonoEditText).setEnabled(false);
+            }
+
+            @Override
+            protected Void doInBackground(Cliente... params) {
+                SQLiteDatabase data = db.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+                //values.put(KEY_ID, 1);
+                values.put(KEY_NOMBRE, params[0].getNombre());
+                values.put(KEY_DIRECCION, params[0].getDireccion());
+                values.put(KEY_TELEFONO, params[0].getTelefono());
+
+                data.insert(TABLE_CLIENTES, null, values);
+                data.close();
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                //Toast.makeText(context, "Insertado", Toast.LENGTH_SHORT).show();
+                //super.onPostExecute(aVoid);
+                activity.findViewById(R.id.nombreEditText).setEnabled(true);
+                activity.findViewById(R.id.direccionEditText).setEnabled(true);
+                activity.findViewById(R.id.telefonoEditText).setEnabled(true);
+        }
     }
 }
